49cac9a29299e1d0096336f46f09108b42cf968b
Ionut Georgescu <ionut.georgescu@uci.edu>
2011-11-18 15:41:20 -0800

    Search LD_LIBRARY_PATH for CHOLMOD too.

--- FindCHOLMOD.cmake
+++ FindCHOLMOD.cmake
@@ -1,5 +1,11 @@
+if (APPLE)
+	set(_libdir ENV DYLD_LIBRARY_PATH)
+else()
+	set(_libdir ENV LD_LIBRARY_PATH)
+endif()
+
 if(NOT CHOLMOD_LIBS)
-	find_library(CHOLMOD_LIBS NAMES cholmod)
+	find_library(CHOLMOD_LIBS NAMES cholmod PATHS ${_libdir})
 	if (NOT CHOLMOD_LIBS)
 		message(FATAL_ERROR "CHOLMOD is needed!")
 	endif()
@@ -15,13 +21,13 @@ endforeach()
 
 find_library(metis_lib NAMES metis PATHS ${CHOLMOD_HOME}/lib NO_DEFAULT_PATH)
 if (NOT metis_lib)
-	find_library(metis_lib NAMES metis)
+	find_library(metis_lib NAMES metis PATHS ${_libdir})
 endif()
 if (metis_lib)
 	set(CHOLMOD_LIBS ${CHOLMOD_LIBS} ${metis_lib})
 endif()
 
-find_path(CHOLMOD_INC cholmod.h PATH_SUFFIXES ufsparse HINTS ${CHOLMOD_HOME}/include)
+find_path(CHOLMOD_INC cholmod.h PATH_SUFFIXES ufsparse suitesparse HINTS ${CHOLMOD_HOME}/include)
 
 message(STATUS "Found CHOLMOD in ${CHOLMOD_HOME}")
 message(STATUS "\theaders in ${CHOLMOD_INC}")
