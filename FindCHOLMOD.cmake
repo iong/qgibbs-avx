a03b6735489dc15db334a3fb8eaaed8289d882b0
Ionut Georgescu <ionut.georgescu@uci.edu>
2011-12-05 16:44:23 -0800

    Search for MKL in a separate file

--- FindCHOLMOD.cmake
+++ FindCHOLMOD.cmake
@@ -5,25 +5,25 @@ else()
 endif()
 
 find_library(CHOLMOD_LIBRARIES NAMES cholmod PATHS ${_libdir})
-if (CHOLMOD_LIBRARIES)
-
-	get_filename_component(CHOLMOD_LIBDIR ${CHOLMOD_LIBRARIES} PATH)
-	get_filename_component(CHOLMOD_HOME ${CHOLMOD_LIBDIR} PATH)
-
-
-	foreach(lib amd camd colamd ccolamd)
-		find_library(${lib}_lib NAMES "${lib}" PATHS ${CHOLMOD_HOME}/lib NO_DEFAULT_PATH)
-		set(CHOLMOD_LIBRARIES ${CHOLMOD_LIBRARIES} ${${lib}_lib})
-	endforeach()
-
-	find_library(metis_lib NAMES metis PATHS ${_libdir})
-	if (metis_lib)
-		set(CHOLMOD_LIBRARIES ${CHOLMOD_LIBRARIES} ${metis_lib})
-	endif()
-
-	find_path(CHOLMOD_INC cholmod.h PATH_SUFFIXES ufsparse suitesparse HINTS ${CHOLMOD_HOME}/include)
-
-	message(STATUS "Found CHOLMOD in ${CHOLMOD_HOME}")
-	message(STATUS "\theaders in ${CHOLMOD_INC}")
-
+if (NOT CHOLMOD_LIBRARIES)
+	return()
 endif()
+
+get_filename_component(CHOLMOD_LIBDIR ${CHOLMOD_LIBRARIES} PATH)
+get_filename_component(CHOLMOD_HOME ${CHOLMOD_LIBDIR} PATH)
+
+
+foreach(lib amd camd colamd ccolamd)
+	find_library(${lib}_lib NAMES "${lib}" PATHS ${CHOLMOD_HOME}/lib NO_DEFAULT_PATH)
+	set(CHOLMOD_LIBRARIES ${CHOLMOD_LIBRARIES} ${${lib}_lib})
+endforeach()
+
+find_library(metis_lib NAMES metis PATHS ${_libdir})
+if (metis_lib)
+	set(CHOLMOD_LIBRARIES ${CHOLMOD_LIBRARIES} ${metis_lib})
+endif()
+
+find_path(CHOLMOD_INC cholmod.h PATH_SUFFIXES ufsparse suitesparse HINTS ${CHOLMOD_HOME}/include)
+
+message(STATUS "Found CHOLMOD in ${CHOLMOD_HOME}")
+message(STATUS "\theaders in ${CHOLMOD_INC}")
