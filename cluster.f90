49b8d3dfa8baff09ee5d232831cecc440e55b29e
Ionuț Georgescu <ionutg@gmail.com>
2012-07-14 17:48:05 -0700

    Write results to different columns

--- cluster.f90
+++ cluster.f90
@@ -38,14 +38,15 @@ program cluster
     call cpu_time(begtime(2))
     do i = 1,Npts
         sf = 0.95 + (1.5-0.95)*real(i-1)/real(Npts-1)
-        call vgwref0(r0 * sf, bl, beta, U(i, 1))
+        r = r0*sf
+        call vgwref0(r, bl, beta, U(i, 2))
     end do
     call cpu_time(endtime(2))
     call vgwrefcleanup()
 
     write (*,*) endtime - begtime
     open(33,file='Uref.dat')
-    write(33, *) (0.95 + (1.5-0.95)*real(i-1)/real(Npts-1), U(i,:), i=1,Npts)
+    write(33, '(3F16.8)') (0.95 + (1.5-0.95)*real(i-1)/real(Npts-1), U(i,:), i=1,Npts)
     close(33)
     
 end program cluster
